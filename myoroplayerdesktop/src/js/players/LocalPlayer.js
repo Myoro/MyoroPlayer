d161a81d5c6c3b6765a444a6cb1fabee255e144d
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2023-08-24 19:54:08 -0600

    I am forever tired

--- myoroplayerdesktop/src/js/players/LocalPlayer.js
+++ myoroplayerdesktop/src/js/players/LocalPlayer.js
@@ -1,10 +1,11 @@
 import Store from "../Store.js";
+import { getShuffleRepeat, oneArgIpcCall } from "../Functions.js";
 
 const player   = new Audio();
 player.volume  = 0.5;
 const queue    = [];
 const cache    = [];
-var playlist   = null; // Playlist used to get new songs from (i.e. next song)
+var playlist   = { directory: null, songs: [] }; // Playlist used to get new songs from (i.e. next song)
 
 export function addToQueue(directory) { queue.push(directory); }
 
@@ -21,17 +22,73 @@ export function togglePlay() {
 }
 
 // When a user double clicks a song
-export function directPlay(songDirectory, playlistDirectory) {
+export async function directPlay(songDirectory, playlistDirectory) {
   cacheSong();
-  playlist = playlistDirectory;
   playSong(songDirectory, 1);
+  playlist.directory = playlistDirectory;
+  playlist.songs     = await oneArgIpcCall("openPlaylist", playlistDirectory);
 }
 export function previousPlay() {
   if(cache.length === 0) return;
   playSong(cache.pop(), 0);
 }
-export function nextPlay() {
-  alert("Hello tomorrow's me");
+export async function nextPlay() {
+  if(queue.length > 0) {
+    cacheSong();
+    playSong(queue.pop(), 1);
+    Store.dispatch({ type: "setPlaySrc", payload: "playing" });
+    return;
+  }
+
+  const { shuffle, repeat } = await getShuffleRepeat();
+
+  // eslint-disable-next-line
+  if(repeat == 1 && player.src) {
+    player.currentTime = 0;
+    player.play();
+    Store.dispatch({ type: "setPlaySrc", payload: "playing" });
+    return;
+  }
+
+  if(playlist.directory === null) return;
+
+  let lastPlaylistSongIndex = null;
+  const songs               = [];
+  for(let i = 0; i < playlist.songs.length; i++)
+    songs.push(playlist.songs[i].songDirectory);
+
+  if(player.src) {
+    const currentSong = decodeURIComponent(player.src.substr(7, player.src.length));
+    if(currentSong.includes(playlist.directory))
+      lastPlaylistSongIndex = songs.indexOf(currentSong);
+  }
+
+  if(lastPlaylistSongIndex === null) {
+    for(let i = 0; i < cache.length; i++) {
+      if(cache[i].includes(playlist.directory)) {
+        lastPlaylistSongIndex = songs.indexOf(cache[i]);
+        break;
+      }
+    }
+
+    if(lastPlaylistSongIndex === null) return;
+  }
+
+  // eslint-disable-next-line
+  let nextSongIndex;
+  if(shuffle == 0) {
+    if(lastPlaylistSongIndex === (songs.length - 1))
+      nextSongIndex = 0;
+    else
+      nextSongIndex = lastPlaylistSongIndex + 1;
+  } else {
+    do {
+      nextSongIndex = Math.floor(Math.random() * songs.length);
+    } while(nextSongIndex === lastPlaylistSongIndex)
+  }
+
+  cacheSong();
+  playSong(songs[nextSongIndex], 1);
 }
 
 // Caches player.src
