33a04660782d01e398c78d84501b54ecf0a8aa7b
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2024-02-22 21:12:44 -0300

    Add backend

---
+++ backend/init.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (request, resolution) => {
+  res.send('Hello, World!');
+});
+
+app.list(PORT, () => {
+  console.log('Server started on port ${PORT}');
+});
