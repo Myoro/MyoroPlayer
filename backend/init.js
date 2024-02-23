a8d758865af7c9055f4e36f6128f4a54756a498d
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2024-02-22 21:21:33 -0300

    update init.js

--- backend/init.js
+++ backend/init.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const HOST = '192.168.0.94';
 
 app.get('/', (request, resolution) => {
   resolution.send('Hello, World!');
 });
 
-app.listen(PORT, () => {
-  console.log('Server started on port ${PORT}');
+app.listen(PORT, HOST, () => {
+  console.log(`Server started on http://${HOST}:${PORT}`);
 });
