50fcb6b84f2c07a007006f654ea5b403e6c986ce
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2024-03-19 20:01:07 -0300

    Now im motivated

--- myoro_player/README.md
+++
@@ -1,3 +0,0 @@
-# myoro_player
-
-A new Flutter project.
--- myoro_player/android/app/src/main/kotlin/com/example/myoro_player/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.myoro_player
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
